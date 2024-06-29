a764d475f8cb85b3502c1f531c15cbfcda9e2eaf
gusCreator <lgsc21211@gmail.com>
2024-06-28 21:20:36 -0500

    Se rutea el componente para testearlo en la web, asimismo, se provee del HTTpCLient, con fetch ya que es recomendado para site server rendering

--- kioskios_web/src/app/app.routes.ts
+++ kioskios_web/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DinamicFormComponent } from './dinamic-form/dinamic-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'forms', component: DinamicFormComponent}
+];
