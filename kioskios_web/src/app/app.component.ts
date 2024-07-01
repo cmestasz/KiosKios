955bcd6d066b933475939d6cae5548fcafde09e7
gusCreator <lgsc21211@gmail.com>
2024-07-01 18:34:22 -0500

    Se coloca un `router-outlet` en el componente principal y se delega los componentes de lo que llamamos la *página principal* al componente `home`. De esta manera, se podrán manejar las rutas adecuadamente desde el componente `home`

--- kioskios_web/src/app/app.component.ts
+++ kioskios_web/src/app/app.component.ts
@@ -1,22 +1,13 @@
-import { Component, ViewChild } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
-import { HeaderComponent } from './home/header/header.component';
-import { NavComponent } from './home/nav/nav.component';
-import { FooterComponent } from './home/footer/footer.component';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, FontAwesomeModule, HeaderComponent, NavComponent, FooterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
   title = 'kioskios_web';
-  constructor(library: FaIconLibrary) {
-    library.addIcons(faCartShopping);
-  }
 }
--- kioskios_web/src/app/app.routes.ts
+++ kioskios_web/src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import { Routes } from '@angular/router';
 import { DinamicFormComponent } from './dinamic-form/dinamic-form.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-  {path: 'forms', component: DinamicFormComponent}
+  {
+    path: 'forms', component: DinamicFormComponent
+  },
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  }
 ];
