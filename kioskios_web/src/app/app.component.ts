480f57f50a6c43f727969a12a6be476b07e5c7f8
gusCreator <lgsc21211@gmail.com>
2024-06-30 19:22:12 -0500

    Se crea una estructura para la página principal de la aplicaicón, sin embargo, luego se componetizará la página en footer, header, nav, etc

--- kioskios_web/src/app/app.component.ts
+++ kioskios_web/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'kioskios_web';
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faCartShopping);
+  }
 }
