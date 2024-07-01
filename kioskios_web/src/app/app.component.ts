b42ee88abb7455569e95212c7c8ae050b9c82281
gusCreator <lgsc21211@gmail.com>
2024-07-01 18:24:51 -0500

    Se añaden los componentes `nav` y `footer` al componente principal.

--- kioskios_web/src/app/app.component.ts
+++ kioskios_web/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { HeaderComponent } from './home/header/header.component';
+import { NavComponent } from './home/nav/nav.component';
+import { FooterComponent } from './home/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, FontAwesomeModule, HeaderComponent],
+  imports: [RouterOutlet, RouterLink, FontAwesomeModule, HeaderComponent, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
