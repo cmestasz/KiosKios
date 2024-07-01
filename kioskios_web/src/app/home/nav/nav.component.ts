977cd7b37fe3bd585c53f15ac98b94bd38610369
gusCreator <lgsc21211@gmail.com>
2024-07-01 18:01:46 -0500

    Se crea un componente para la página principla llamado `home` y subcomponentes que son `header`, `nav` y `footer`. Asimsimo, se creo un componente `login` para el inicio de sesión de los usuarios.

---
+++ kioskios_web/src/app/home/nav/nav.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      nav works!
+    </p>
+  `,
+  styles: ``
+})
+export class NavComponent {
+
+}
