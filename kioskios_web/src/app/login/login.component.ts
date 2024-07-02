af186cb76d41fdc05fbdfd393dda0beb421df8c8
gusCreator <lgsc21211@gmail.com>
2024-07-01 20:07:31 -0500

    Se crea un cargador del formulario para que permita cargarlo dinamicamente. Luego lo referncia y le aplico el método que necesita para cargar de acuerdo al formulario requerido.

--- kioskios_web/src/app/login/login.component.ts
+++ kioskios_web/src/app/login/login.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ComponentRef, ViewContainerRef } from '@angular/core';
+import { DinamicFormComponent } from '../dinamic-form/dinamic-form.component';
+
+@Component({
+  selector: 'loader-form',
+  standalone: true,
+  imports: [DinamicFormComponent],
+  template: `
+    
+  `,
+})
+
+export class LoaderForm {
+
+  private componentRef: ComponentRef<DinamicFormComponent>;
+
+  constructor(
+    private viewContainer: ViewContainerRef,
+  ){
+    this.componentRef =  this.viewContainer.createComponent(DinamicFormComponent);
+    if(this.componentRef && this.componentRef.instance){
+      this.componentRef.instance.loadSchema("login");
+    }
+  }
+
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [LoaderForm],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
