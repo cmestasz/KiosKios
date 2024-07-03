e1e7bdd2c01d622cd8e001e9f1b0b5e6d521b112
gusCreator <lgsc21211@gmail.com>
2024-07-02 19:13:46 -0500

    Se realizan las peticiones a la api desde el servicio

--- kioskios_web/src/app/services/api.service.ts
+++ kioskios_web/src/app/services/api.service.ts
@@ -1,28 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
 import { FormField } from '../models/form-field';
+import { response } from 'express';
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
 
-  private urlApi = 'http://localhost:8000/api/';
-  campos1: FormField[] = [
-    { tipoCampo: 'input', name: 'nombre', label: 'Nombre', type: 'text', validators: {maxlength: '10'} },
-  ];
-  campos2: FormField[] = [
-    { tipoCampo: 'textarea', name: 'text', label: 'Texto', type: 'text', validators: {maxlength: '10'} },
-  ];
-
+  private urlBaseApi : string = 'http://localhost:8000/api';
 
   constructor(private http: HttpClient) { }
 
   getFormSchema(formToGet : string) : Observable<FormField[]> {
-    const url = `${this.urlApi}create_${formToGet}`;
-    // return this.http.get<FormField[]>(url);
-    if(formToGet == 'login')
-      return of(this.campos1);
-    return of(this.campos2);
+    const url = this.urlBaseApi + `/create_${formToGet}`;
+    return this.http.get<{campos: FormField[]}>(url).pipe(
+      map(response => response.campos)
+    );
   }
 }
