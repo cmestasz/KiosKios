4ab22adcb192ccb42c4d904925344e7326743dc6
gusCreator <lgsc21211@gmail.com>
2024-07-02 22:42:15 -0500

    En el login se creará el formulario del login. Aun no se ha implementado la capacidad de redirigir o de hacer submit.

--- kioskios_web/src/app/login/login.component.ts
+++ kioskios_web/src/app/login/login.component.ts
@@ -1,38 +1,25 @@
-import { Component, ComponentRef, ViewContainerRef } from '@angular/core';
-import { DinamicFormComponent } from '../dinamic-form/dinamic-form.component';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { LoaderFormComponent } from '../dinamic-form/loader-form/loader-form.component';
 import { TYPE_FORMS } from '../constants';
 
-@Component({
-  selector: 'loader-form',
-  standalone: true,
-  imports: [DinamicFormComponent],
-  template: `
-    
-  `,
-})
 
-export class LoaderForm {
-
-  private componentRef: ComponentRef<DinamicFormComponent>;
-
-  constructor(
-    private viewContainer: ViewContainerRef,
-  ){
-    this.componentRef =  this.viewContainer.createComponent(DinamicFormComponent);
-    if(this.componentRef && this.componentRef.instance){
-      this.componentRef.instance.loadSchema(TYPE_FORMS.USER);
-    }
-  }
-
-}
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LoaderForm],
+  imports: [LoaderFormComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
+
+  @ViewChild(LoaderFormComponent) loaderForm!: LoaderFormComponent;
+
+  ngAfterViewInit(): void {
+    if(this.loaderForm)
+      this.loaderForm.createForm(TYPE_FORMS.LOGIN);
+    else
+      console.log("No se pudo cargar el formulario de login");
+  }
 
 }
