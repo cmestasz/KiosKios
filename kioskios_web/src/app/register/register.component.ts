a1bf49839b49fa6c9ff7e9938436a5e7749e24c9
gusCreator <lgsc21211@gmail.com>
2024-07-03 22:23:15 -0500

    Se crean las funciones que cargan los formularios de registro dependiendo de la persona que quiera registrarse

--- kioskios_web/src/app/register/register.component.ts
+++ kioskios_web/src/app/register/register.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { LoaderFormComponent } from '../dinamic-form/loader-form/loader-form.component';
+import { TYPE_FORMS } from '../constants';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [LoaderFormComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  @ViewChild(LoaderFormComponent) loaderForm!: LoaderFormComponent;
+
+  loadOwner() : void {
+    this.loaderForm.createForm(TYPE_FORMS.CREATE_OWNER);
+  }
+
+  loadUser() : void {
+    this.loaderForm.createForm(TYPE_FORMS.CREATE_USER);
+  }
 
 }
