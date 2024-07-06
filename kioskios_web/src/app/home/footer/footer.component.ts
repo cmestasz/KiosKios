e8fdebcaa7828b6519441ac77fd47c3048b86267
dmolloc1 <dmolloc@unsa.edu.pe>
2024-07-05 22:52:30 -0500

    Se agrego la estructura html con clases en el componente footer

--- kioskios_web/src/app/home/footer/footer.component.ts
+++ kioskios_web/src/app/home/footer/footer.component.ts
@@ -5,7 +5,29 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <footer></footer>
+    <footer class="footer">
+        <div class="container-footer">
+            <div class="row">
+                <div>
+                    <h2 class="footer-heading logo">Kios Kios</h2>
+                    <p class="menu">
+                        <a href="#">Home</a>
+                        <a href="#">About</a>
+                        <a href="#">Coments</a>
+                        <a href="#">All</a>
+                        <a href="#">Contact</a>
+                    </p>
+                    <ul class="ftco-footer-social ">
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <p>
+                    Kioskios | <script>document.write(new Date().getFullYear());</script> Sigue disfrutando de tus compras en KiosKios.com
+                </p>
+            </div>
+        </div>
+    </footer>
   `,
   styles: ``
 })
