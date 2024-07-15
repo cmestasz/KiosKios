2942a1ad60cc383012be390aa9aceb07d5efd2ba
dmolloc1 <dmolloc@unsa.edu.pe>
2024-07-15 07:58:38 -0500

    Se agrego el estilo a la pagina de inicio de seción

--- kioskios_web/src/app/login/login.component.ts
+++ kioskios_web/src/app/login/login.component.ts
@@ -1,13 +1,14 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { LoaderFormComponent } from '../dinamic-form/loader-form/loader-form.component';
 import { TYPE_FORMS } from '../constants';
-
+import { FooterComponent } from '../home/footer/footer.component';
+import { HeaderComponent } from '../home/header/header.component';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LoaderFormComponent],
+  imports: [LoaderFormComponent, FooterComponent, HeaderComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
