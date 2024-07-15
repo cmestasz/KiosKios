a5fb5d5dd3be9de7ba5ff0c32ca7b5c0375c19ae
gusCreator <lgsc21211@gmail.com>
2024-07-15 15:56:53 -0500

    Se crea un componente que representa la página para un usuario cualquiera que no está loggeado

---
+++ kioskios_web/src/app/home/any-home/any-home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { BannerComponent } from '../banner/banner.component';
+
+@Component({
+  selector: 'app-any-home',
+  standalone: true,
+  imports: [BannerComponent],
+  templateUrl: './any-home.component.html',
+  styleUrl: './any-home.component.css'
+})
+export class AnyHomeComponent {
+
+}
