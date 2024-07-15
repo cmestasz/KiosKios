685cc89715d7c84a4679b690bdd323331b50a383
gusCreator <lgsc21211@gmail.com>
2024-07-15 15:55:20 -0500

    Se crean los componentes para las páginas de usuario y de dueño

---
+++ kioskios_web/src/app/home/owner-home/owner-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-owner-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './owner-home.component.html',
+  styleUrl: './owner-home.component.css'
+})
+export class OwnerHomeComponent {
+
+}
