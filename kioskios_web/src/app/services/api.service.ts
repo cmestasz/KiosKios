1c9a0d7743a1d623a3dc949704e3aa021eef58f4
gusCreator <lgsc21211@gmail.com>
2024-07-16 21:16:41 -0500

    Se añade un método en la api que hace las peticiones para los datos del usuario.

--- kioskios_web/src/app/services/api.service.ts
+++ kioskios_web/src/app/services/api.service.ts
@@ -3,6 +3,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, catchError, map, of } from 'rxjs';
 import { FormField } from '../models/form-field';
 import { Form } from '../form';
+import { User } from '../models/user';
+import { response } from 'express';
 @Injectable({
   providedIn: 'root',
 })
@@ -11,6 +13,18 @@ export class ApiService {
   private urlBaseApi : string = 'http://localhost:8000/api';
 
   constructor(private http: HttpClient) { }
+
+  getUser(email: string): Observable<User | undefined> {
+    const url = this.urlBaseApi + '/get_usuario_por_correo/';
+    return this.http.post<{status: number, message?: string, usuario?: User}>(url, {email: email}).pipe(
+      map(response => {
+        if (response.status != 200) {
+          return undefined;
+        }
+        return response.usuario;
+      })
+    );
+  }
 
   postForm(formtoSend: FormData, to: string):Observable<any>{
     const url = this.urlBaseApi + `/${to}/`;
