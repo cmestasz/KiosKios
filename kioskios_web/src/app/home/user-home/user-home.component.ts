a80419ce492b52422391eae885f00b6dde972404
gusCreator <lgsc21211@gmail.com>
2024-07-18 16:13:16 -0500

    Se accede a los atributos del servicio de autenticación asíncronamente

--- kioskios_web/src/app/home/user-home/user-home.component.ts
+++ kioskios_web/src/app/home/user-home/user-home.component.ts
@@ -1,25 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../models/user';
+import { EMPTY_USER } from '../../constants';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-home.component.html',
   styleUrl: './user-home.component.css'
 })
-export class UserHomeComponent implements OnInit{
-  user: User | undefined;
+export class UserHomeComponent{
+  user$: Observable<User>;
   constructor(private authService: AuthService){
-    
+    this.user$ = this.authService.getUser();
   }
 
-  ngOnInit(): void {
-    this.authService.getUserLoaded().subscribe(
-      user => {
-        this.user = user; 
-      }
-    );
-  }
 }
