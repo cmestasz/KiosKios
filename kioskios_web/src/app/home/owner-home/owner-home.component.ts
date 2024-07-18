960bb1026fc714b021899f650878dc62c00f597b
gusCreator <lgsc21211@gmail.com>
2024-07-18 16:15:17 -0500

    Se accede al usuario entregado por el servicio de forma asíncrona

--- kioskios_web/src/app/home/owner-home/owner-home.component.ts
+++ kioskios_web/src/app/home/owner-home/owner-home.component.ts
@@ -1,21 +1,19 @@
 import { Component } from '@angular/core';
 import { User } from '../../models/user';
 import { AuthService } from '../../services/auth.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-owner-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './owner-home.component.html',
   styleUrl: './owner-home.component.css'
 })
 export class OwnerHomeComponent {
-  user: User | undefined;
+  user$: Observable<User>;
   constructor(private authService: AuthService){
-    this.authService.getUser().subscribe(
-      user => {
-        this.user = user
-      }
-    );
+    this.user$ = this.authService.getUser();
   }
 }
