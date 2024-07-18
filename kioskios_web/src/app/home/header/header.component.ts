26105f34b1522c8f0028654f9bb68b6a98697405
gusCreator <lgsc21211@gmail.com>
2024-07-17 22:54:05 -0500

    Se escucha desde el servicio para obtener el usuario asíncronamente y renderizarlo

--- kioskios_web/src/app/home/header/header.component.ts
+++ kioskios_web/src/app/home/header/header.component.ts
@@ -2,24 +2,41 @@ import { Component } from '@angular/core';
 import { RouterLink} from '@angular/router';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCartShopping, faMagnifyingGlass, faBars } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../models/user';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'home-header',
   standalone: true,
-  imports: [RouterLink, FontAwesomeModule],
+  imports: [RouterLink, FontAwesomeModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 
 })
 export class HeaderComponent {
   isMenuOpen: boolean = false;
-  constructor(library: FaIconLibrary){
+  user$: Observable<User | undefined>
+  constructor(
+    library: FaIconLibrary,
+    private authService: AuthService
+  ){
     library.addIcons(faCartShopping, faMagnifyingGlass);
+    this.user$ = this.authService.getUserAsObservable();
   }
   
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  signOut() {
+    // Cerrar sesión del usuario
+  }
+
+  showSignOut() {
+    
   }
   
 }
