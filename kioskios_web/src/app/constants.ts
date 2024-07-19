4defc1d2f135bfb01ad42cff1c1944db8e8f68dc
Christian <cmestasz@unsa.edu.pe>
2024-07-19 09:25:46 -0500

    Se crea un prototipo de vista de productos

--- kioskios_web/src/app/constants.ts
+++ kioskios_web/src/app/constants.ts
@@ -6,7 +6,8 @@ export const TYPE_FORMS = {
   CREATE_STORE: "crear_tienda",
   CREATE_PRODUCT: "crear_producto",
   CREATE_VENTA: "crear_venta",
-  LOGIN: "iniciar_sesion"
+  LOGIN: "iniciar_sesion",
+  GET_PRODUCTS: "get_productos"
 };
 
 export const EMPTY_USER = {} as User;
