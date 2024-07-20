266b8e4c956b2d36d424dbcffc860fcdd278ef3a
gusCreator <lgsc21211@gmail.com>
2024-07-19 21:53:55 -0500

    Se muestran todos los productos de las tiendas al usuario

--- kioskios_web/src/app/home/user-home/user-home.component.ts
+++ kioskios_web/src/app/home/user-home/user-home.component.ts
@@ -5,20 +5,26 @@ import { EMPTY_USER } from '../../constants';
 import { filter, map, Observable, tap } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { Tienda } from '../../tienda';
+import { ProductListComponent } from '../../product-list/product-list.component';
 
 @Component({
   selector: 'app-user-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProductListComponent],
   templateUrl: './user-home.component.html',
   styleUrl: './user-home.component.css'
 })
 export class UserHomeComponent{
-  user: User = EMPTY_USER;
+  user: User;
+  tiendas!: Tienda[];
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private api: ApiService
   ){
+    this.user = EMPTY_USER;
     this.authService.getUser().pipe(
       tap(
         user => {
@@ -34,6 +40,13 @@ export class UserHomeComponent{
         console.log("Recibiendo usuario en el componente User: " + user);    
         this.user = user;
     });
+
+    this.api.getTiendas().subscribe(
+      tiendas => {
+        this.tiendas = tiendas;
+      }
+    );
+
   }
 
 }
