9c94579b9cab70a70fe61d8cf65903d48d38ed8f
Christian <cmestasz@unsa.edu.pe>
2024-07-20 14:04:09 -0500

    Puerto

--- kioskios_api/kioskios_api/settings.py
+++ kioskios_api/kioskios_api/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': os.environ.get('6543'),
+        'PORT': '6543',
     }
 }
 
