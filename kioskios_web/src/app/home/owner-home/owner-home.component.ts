b4a2f1aab824782543797a367ff90203557ebc37
gusCreator <lgsc21211@gmail.com>
2024-07-19 21:52:42 -0500

    Se modifica la página del propiertario para que utilice correctamente los productos de la api

--- kioskios_web/src/app/home/owner-home/owner-home.component.ts
+++ kioskios_web/src/app/home/owner-home/owner-home.component.ts
@@ -1,9 +1,5 @@
 import { Component, HostListener } from '@angular/core';
-import { User } from '../../models/user';
-import { AuthService } from '../../services/auth.service';
-import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { MODEL_URL, TYPE_FORMS } from '../../constants';
 import { Producto } from '../../models/product';
 import { Router, RouterLink } from '@angular/router';
 import { ApiService } from '../../services/api.service';
@@ -18,7 +14,7 @@ import { ApiService } from '../../services/api.service';
 export class OwnerHomeComponent {
   productos: Producto[] = [];
   constructor(private apiService: ApiService, private router: Router) {
-    this.apiService.getModel<Producto>(MODEL_URL.PRODUCTO).subscribe((objects) => {
+    this.apiService.getProducts().subscribe((objects) => {
       this.productos = objects;
     });
   }
