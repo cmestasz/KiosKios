44d2c4e5b47a73c744ee5fd5f722c0defd65816f
gusCreator <lgsc21211@gmail.com>
2024-07-21 15:15:00 -0500

    Se cambian las rutas para hacer un correcto filtrado

--- kioskios_web/src/app/app.routes.ts
+++ kioskios_web/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { AnyHomeComponent } from './home/any-home/any-home.component';
 import { HomeComponent } from './home/home.component';
 import { AuthRedirectComponent } from './auth-redirect/auth-redirect.component';
 import { ProductDetailsComponent } from './home/product-details/product-details.component';
+import { FilterUserRouteComponent } from './home/filter-user-route/filter-user-route.component';
 
 export const routes: Routes = [
   {
@@ -16,21 +17,28 @@ export const routes: Routes = [
         path: '', component: AnyHomeComponent, title: 'KiosKios - Tu tienda virtual'
       },
       {
-        path: 'user', children: [
+        path: 'dashboard', component: FilterUserRouteComponent, children: [
           {
-            path: '', title: 'Home - Usuario', component: UserHomeComponent
+            path: 'user', children: [
+              {
+                path: '', title: 'Home - Usuario', component: UserHomeComponent
+              },
+              {
+                path: 'product/:id', component: ProductDetailsComponent, title: 'Ver detalles del producto'
+              }
+            ]
           },
           {
-            path: 'product/:id', component: ProductDetailsComponent, title: 'Ver detalles del producto'
+            path: 'owner', children: [
+              {
+                path: '', component: OwnerHomeComponent, title: 'Home - Dueño'
+              }
+            ]
           }
         ]
       },
       {
-        path: 'owner', children: [
-          {
-            path: '', component: OwnerHomeComponent, title: 'Home - Dueño'
-          }
-        ]
+
       }
     ]
   },
