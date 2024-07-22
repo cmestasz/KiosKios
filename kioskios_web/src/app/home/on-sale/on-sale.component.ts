6160ed9fe374112781d4aa5bef87b5d7185777f2
gusCreator <lgsc21211@gmail.com>
2024-07-21 21:52:14 -0500

    Se agrega una vista preliminar para las ventas

--- kioskios_web/src/app/home/on-sale/on-sale.component.ts
+++ kioskios_web/src/app/home/on-sale/on-sale.component.ts
@@ -3,6 +3,7 @@ import { Venta } from '../../models/venta';
 import { SalesService } from '../../services/sales.service';
 import { EMPTY_SALE } from '../../constants';
 import { ApiService } from '../../services/api.service';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-on-sale',
@@ -12,11 +13,13 @@ import { ApiService } from '../../services/api.service';
   styleUrl: './on-sale.component.css'
 })
 export class OnSaleComponent implements OnInit{
-  sale: Venta
+  sale: Venta;
+  qr!: SafeUrl;
 
   constructor(
     private salesService: SalesService,
-    private api: ApiService
+    private api: ApiService,
+    private sanitizer: DomSanitizer
   ) {
     this.sale = EMPTY_SALE;
 
@@ -26,7 +29,19 @@ export class OnSaleComponent implements OnInit{
     this.salesService.getCurrentSale().subscribe(
       sale => {
         this.sale = sale;
-        this.api.getShopById
+        this.api.getQrByShop(this.sale.tiendaId).subscribe(
+          url => {
+            this.api.getMedia(url).subscribe({
+              next: (imageBlob) => {
+                const objectUrl = URL.createObjectURL(imageBlob);
+                this.qr = this.sanitizer.bypassSecurityTrustUrl(objectUrl);
+              },
+              error: err => {
+                throw new Error(err);
+              }
+            });
+          }
+        )
       }
     );
   }
