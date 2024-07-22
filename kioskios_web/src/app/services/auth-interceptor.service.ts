a7c470c557f977337cae3ac06799322a5fae3f94
gusCreator <lgsc21211@gmail.com>
2024-07-21 21:47:02 -0500

    Se elimina el token y el usuario del caché cuando se acaba su sesión.

--- kioskios_web/src/app/services/auth-interceptor.service.ts
+++ kioskios_web/src/app/services/auth-interceptor.service.ts
@@ -13,11 +13,16 @@ export class AuthInterceptorService implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log("Interceptando este error: ", req);
+    if (req.body.status) {
+      
+    }
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        console.log("Interceptando error de autenticación");
+        console.log("Interceptando error de autenticación: ", error);
         if(error.status === 401) {
           const returnUrl = this.router.url;
+          localStorage.removeItem('user');
+          localStorage.removeItem('token');
           this.router.navigate(['/login'], { queryParams: {returnUrl} })
         }
         throw error;
