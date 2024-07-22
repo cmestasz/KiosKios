e7281e4087681994debae9418b7cf30f1eb4d345
Christian <cmestasz@unsa.edu.pe>
2024-07-22 01:01:10 -0500

    conexion

--- kioskios_api/kioskios_api/db_storage.py
+++ kioskios_api/kioskios_api/db_storage.py
@@ -1,5 +1,5 @@
 from django.core.files.storage import Storage
-from supabase import create_client
+from supabase import create_client, ClientOptions
 import os
 
 
@@ -7,8 +7,12 @@ class DBStorage(Storage):
 
     def __init__(self, option=None):
         url = os.environ.get('SUPABASE_URL')
-        key = os.environ.get('SUPABASE_ANON_KEY')
-        self.client = create_client(url, key)
+        key = os.environ.get('DB_PASSWORD')
+        options = ClientOptions(
+            postgrest_client_timeout=7000,
+            storage_client_timeout=7000
+        )
+        self.client = create_client(url, key, options)
 
     def _open(self, name, mode='rb'):
         data = self.client.storage.from_('media').download(name)
